06507893bb56417e1a86ed1c28dce619c17af3bd
Ken McDonald <ken.mcdonald@checkmarx.com>
2019-11-22 16:59:41 -0500

    Adding unit test for Config as Code parsing

---
+++ src/test/java/com/checkmarx/sdk/utils/ScanUtilsTest.java
@@ -0,0 +1,22 @@
+package com.checkmarx.sdk.utils;
+
+import com.checkmarx.sdk.dto.CxConfig;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+public class ScanUtilsTest {
+
+    @Test
+    public void testGetConfigAsCode() {
+        File file = new File(
+            getClass().getClassLoader().getResource("CxConfig.json").getFile()
+        );
+        CxConfig config = ScanUtils.getConfigAsCode(file);
+        assertNotNull(config);
+        assertTrue(config.getActive());
+        assertEquals("/a/b/c", config.getTeam());
+    }
+}
