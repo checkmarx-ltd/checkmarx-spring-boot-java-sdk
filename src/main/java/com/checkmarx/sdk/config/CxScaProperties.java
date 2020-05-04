6410d223b53b1d9c4104c683efbf396e522cb548
NimrodGolan <nimrod.golan@checkmarx.com>
2020-05-04 12:56:26 +0300

    Create new SCA properties prefix

---
+++ src/main/java/com/checkmarx/sdk/config/CxScaProperties.java
@@ -0,0 +1,22 @@
+package com.checkmarx.sdk.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@ConfigurationProperties(prefix = "sca")
+@Validated
+@Getter
+@Setter
+public class CxScaProperties {
+
+    private String appUrl;
+    private String apiUrl;
+    private String accessControlUrl;
+    private String tenant;
+    private String username;
+    private String password;
+}
