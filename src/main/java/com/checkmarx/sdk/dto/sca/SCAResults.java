2026a5e65cda847638090f393f6da2bdd3712a9b
AlexeyK <alexey.kononov@checkmarx.com>
2020-05-07 12:00:34 +0300

    Added getter/setter.

--- src/main/java/com/checkmarx/sdk/dto/sca/SCAResults.java
+++ src/main/java/com/checkmarx/sdk/dto/sca/SCAResults.java
@@ -2,10 +2,14 @@ package com.checkmarx.sdk.dto.sca;
 
 import com.checkmarx.sdk.dto.Filter;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Map;
 
 @Builder
+@Getter
+@Setter
 public class SCAResults {
     private int totalPackages;
     private int directPackages;
