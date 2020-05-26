64d4c5499abd42462cbf3ed17b6c4df32a790f4a
AlexeyK <alexey.kononov@checkmarx.com>
2020-05-26 11:29:04 +0300

    Extracted preset-related methods.

--- src/main/java/com/checkmarx/sdk/service/ScanSettingsClientImpl.java
+++ src/main/java/com/checkmarx/sdk/service/ScanSettingsClientImpl.java
@@ -2,7 +2,9 @@ package com.checkmarx.sdk.service;
 
 import com.checkmarx.sdk.config.Constants;
 import com.checkmarx.sdk.config.CxProperties;
+import com.checkmarx.sdk.dto.cx.CxPreset;
 import com.checkmarx.sdk.dto.cx.CxScanSettings;
+import com.checkmarx.sdk.exception.CheckmarxException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +25,7 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 public class ScanSettingsClientImpl implements ScanSettingsClient {
     private static final String SCAN_SETTINGS = "/sast/scanSettings";
+    private static final String PRESETS = "/sast/presets";
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private final RestTemplate restTemplate;
@@ -89,5 +92,73 @@ public class ScanSettingsClientImpl implements ScanSettingsClient {
             log.error("Error parsing scan settings response.", e);
         }
         return result;
+    }
+
+    @Override
+    public Integer getPresetId(String presetName) throws CheckmarxException {
+        HttpEntity httpEntity = new HttpEntity<>(authClient.createAuthHeaders());
+        int defaultPresetId = Constants.UNKNOWN_INT;
+        try {
+            log.info("Retrieving Cx presets");
+            ResponseEntity<CxPreset[]> response = restTemplate.exchange(cxProperties.getUrl().concat(PRESETS), HttpMethod.GET, httpEntity, CxPreset[].class);
+            CxPreset[] cxPresets = response.getBody();
+            if (cxPresets == null) {
+                throw new CheckmarxException("Error obtaining Team Id");
+            }
+            for (CxPreset cxPreset : cxPresets) {
+                String currentPresetName = cxPreset.getName();
+                int presetId = cxPreset.getId();
+                if (currentPresetName.equalsIgnoreCase(presetName)) {
+                    log.info("Found preset '{}' with ID {}", presetName, presetId);
+                    return cxPreset.getId();
+                }
+                if (currentPresetName.equalsIgnoreCase(Constants.CX_DEFAULT_PRESET)) {
+                    defaultPresetId = presetId;
+                }
+            }
+            log.warn("No Preset was found for '{}'", presetName);
+            log.warn("Default Preset {} with ID {} will be used instead", Constants.CX_DEFAULT_PRESET, defaultPresetId);
+            return defaultPresetId;
+        } catch (HttpStatusCodeException e) {
+            log.error("Error occurred while retrieving presets");
+            log.error(ExceptionUtils.getStackTrace(e));
+            throw new CheckmarxException("Error obtaining Preset Id");
+        }
+    }
+
+    @Override
+    public String getPresetName(int presetId) {
+        HttpEntity requestEntity = new HttpEntity<>(authClient.createAuthHeaders());
+
+        log.info("Retrieving preset name for preset Id {}", presetId);
+        try {
+            ResponseEntity<String> response = restTemplate.exchange(cxProperties.getUrl().concat(PRESETS.concat("/{id}")),
+                    HttpMethod.GET,
+                    requestEntity,
+                    String.class,
+                    presetId);
+
+            if (response.getBody() == null) {
+                return null;
+            }
+            JSONObject obj = new JSONObject(response.getBody());
+            return obj.getString("name");
+        } catch (HttpStatusCodeException e) {
+            log.error("Error occurred while retrieving preset for preset id {}, http error {}", presetId, e.getStatusCode());
+            log.error(ExceptionUtils.getStackTrace(e));
+        } catch (JSONException e) {
+            log.error("Error processing JSON Response");
+            log.error(ExceptionUtils.getStackTrace(e));
+        }
+        return null;
+    }
+
+    @Override
+    public Integer getProjectPresetId(int projectId) {
+        CxScanSettings scanSettings = getScanSettingsDto(projectId);
+        if (scanSettings == null) {
+            return Constants.UNKNOWN_INT;
+        }
+        return scanSettings.getPresetId();
     }
 }
