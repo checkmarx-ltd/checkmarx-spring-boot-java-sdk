fcfb9454da1ad8c158338167220c2fb0c8991456
AlexeyK <alexey.kononov@checkmarx.com>
2020-05-27 15:36:31 +0300

    Class comment.

--- src/main/java/com/checkmarx/sdk/dto/filtering/ScriptInput.java
+++ src/main/java/com/checkmarx/sdk/dto/filtering/ScriptInput.java
@@ -4,6 +4,11 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Passed as input parameter to a filtering script.
+ * Only contains properties that are needed for filtering result evaluation.
+ * May be extended to include properties for other types of filters.
+ */
 @Builder
 @Getter
 @Setter
