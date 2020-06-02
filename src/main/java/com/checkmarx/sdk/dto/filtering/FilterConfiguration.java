ec0e96befb97025a19e2e5da361bc3040fe049b1
AlexeyK <alexey.kononov@checkmarx.com>
2020-06-02 12:50:29 +0300

    Added @ToString for better logging.

--- src/main/java/com/checkmarx/sdk/dto/filtering/FilterConfiguration.java
+++ src/main/java/com/checkmarx/sdk/dto/filtering/FilterConfiguration.java
@@ -4,12 +4,14 @@ import com.checkmarx.sdk.dto.Filter;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @Setter
 @Builder
+@ToString
 public class FilterConfiguration {
     private List<Filter> simpleFilters;
     private ScriptedFilter scriptedFilter;
