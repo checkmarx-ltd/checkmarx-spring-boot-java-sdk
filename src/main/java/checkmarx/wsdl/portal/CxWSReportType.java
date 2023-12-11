d776470ae24d1b77710691039358ca8f79922ff5
satyamchaurasiapersistent <satyam.chaurasia2022@gmail.com>
2023-12-11 14:18:14 +0530

    Spring boot Migration 3.0 changes.

--- src/main/java/checkmarx/wsdl/portal/CxWSReportType.java
+++ src/main/java/checkmarx/wsdl/portal/CxWSReportType.java
@@ -8,8 +8,8 @@
 
 package checkmarx.wsdl.portal;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlType;
+import jakarta.xml.bind.annotation.XmlEnum;
+import jakarta.xml.bind.annotation.XmlType;
 
 
 /**
--- src/main/java/com/checkmarx/sdk/dto/cx/projectdetails/CustomField.java
+++ src/main/java/com/checkmarx/sdk/dto/cx/projectdetails/CustomField.java
@@ -3,7 +3,7 @@ package com.checkmarx.sdk.dto.cx.projectdetails;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
-import javax.annotation.Generated;
+import jakarta.annotation.Generated;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
