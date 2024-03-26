6ce9be8ca9d5ecb58f8bedd3950f6415fb0ad4b4
Satyam Chaurasia <samchaurasia86@gmail.com>
2024-03-26 17:10:36 +0530

    CXFLW-916 scan location overwrite

---
+++ src/main/java/com/checkmarx/sdk/utils/SASTScanReport.java
@@ -0,0 +1,26 @@
+public class SASTScanReport {
+
+    private int scanId;
+    private boolean hasFindings;
+
+    public SASTScanReport(int scanId, boolean hasFindings) {
+        this.scanId = scanId;
+        this.hasFindings = hasFindings;
+    }
+
+    public int getScanId() {
+        return scanId;
+    }
+
+    public void setScanId(int scanId) {
+        this.scanId = scanId;
+    }
+
+    public boolean isHasFindings() {
+        return hasFindings;
+    }
+
+    public void setHasFindings(boolean hasFindings) {
+        this.hasFindings = hasFindings;
+    }
+}
